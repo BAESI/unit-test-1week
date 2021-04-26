9e256d62d6ac642499fa92ec3f5e3044dbab53a1
delta <baesh0408@naver.com>
2021-04-27 01:53:29 +0900

    practice testing

--- src/test/java/CustomCalculatorTest.java
+++ src/test/java/CustomCalculatorTest.java
@@ -1,47 +1,47 @@
-
-import org.junit.Before;
+import junit.framework.TestCase;
 import org.junit.Test;
 
-import static org.hamcrest.CoreMatchers.is;
-import static org.junit.Assert.*;
-
-public class CustomCalculatorTest {
+public class CustomCalculatorTest extends TestCase {
 
     private CustomCalculator customCalculator;
 
-    //더하기 테스트 작성
+    // 더하기 테스트
     @Test
-    public void add() {
+    public void testAdd() {
         customCalculator = new CustomCalculator();
-        int result = customCalculator.add(10,15);
-        /* 이곳에 테스트 코드를 작성하세요. */
-        System.out.println("result :: " + result);
+        int result = customCalculator.add(10, 10);
+        // 테스트 코드 작성
+        assertTrue(result == 20);
+        assertFalse(result == 19);
     }
 
-    //빼기 테스트 작성
+    // 빼기 테스트
     @Test
-    public void subtract() {
+    public void testSubtract() {
         customCalculator = new CustomCalculator();
-        int result = customCalculator.subtract(23,10);
-        /* 이곳에 테스트 코드를 작성하세요. */
-        System.out.println("result :: " + result);
+        int result = customCalculator.subtract(10, 10);
+        // 테스트 코드 작성
+        assertTrue(result == 0);
+        assertFalse(result == 1);
     }
 
-    //곱하기 테스트 작성
+    // 곱하기 테스트
     @Test
-    public void multiply() {
+    public void testMultiply() {
         customCalculator = new CustomCalculator();
-        int result = customCalculator.multiply(5,9);
-        /* 이곳에 테스트 코드를 작성하세요. */
-        System.out.println("result :: " + result);
+        int result = customCalculator.multiply(10, 10);
+        // 테스트 코드 작성
+        assertTrue(result == 100);
+        assertFalse(result == 99);
     }
 
-    //나누기 테스트 작성
+    // 나누기 테스트
     @Test
-    public void divide() {
+    public void testDivide() {
         customCalculator = new CustomCalculator();
-        int result = customCalculator.divide(25,5);
-        /* 이곳에 테스트 코드를 작성하세요. */
-        System.out.println("result :: " + result);
+        int result = customCalculator.divide(10, 10);
+        // 테스트 코드 작성
+        assertTrue(result == 1);
+        assertFalse(result == 2);
     }
 }
